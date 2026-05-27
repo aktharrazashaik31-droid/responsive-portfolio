9027e0ab402361bf43323516cac393733dab28a1
aktharrazashaik31-droid <aktharrazashaik31@gmail.com>
2026-05-27 15:52:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// script.js
+
+const themeToggle =
+document.getElementById('theme-toggle');
+
+const body = document.body;
+
+
+// DARK MODE
+
+themeToggle.addEventListener('click', () => {
+
+    body.classList.toggle('dark-mode');
+
+});
+
+
+// MOBILE MENU
+
+const menuBtn =
+document.getElementById('menu-btn');
+
+const navbar =
+document.getElementById('navbar');
+
+
+menuBtn.addEventListener('click', () => {
+
+    navbar.classList.toggle('active');
+
+});
